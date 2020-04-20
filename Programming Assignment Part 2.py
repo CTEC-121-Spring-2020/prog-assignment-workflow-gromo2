cb92debc548eaa5dd67243ea802fcf825b026e26
Garrett <g.romo2@students.clark.edu>
2020-04-20 14:48:38 -0700

    Programming assignment

---
+++ Programming Assignment Part 2.py
@@ -0,0 +1,12 @@
+"""
+CTEC 121
+<Garrett>
+<TASK: Programming Assignment Part 2>
+<This is the second part to the programming assingment.
+"""
+
+def main():
+    print("<(O_O<)")
+    print("<(O_O)>")
+    print("(>O_O)> Kirby Dance")
+main()
---
+++ Programming Assignment.py
@@ -0,0 +1,18 @@
+"""
+CTEC 121
+<Garrett>
+<TASK: Programming Assignment>
+<This is a repeat of the lab but from your home computer. It shows that you can correctly navigate through the assignment workflow.
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print("Hey, here's some sample text!")
+    print("This is pretty cool, isn't it?")
+    print("You just type stuff, and the computer knows what it means, just like that.")
+main()    
